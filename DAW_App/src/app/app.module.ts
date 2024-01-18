9b6fb2f07467da6cf0848cc521a5adb82827b8b6
Theodor Constantin <113206278+theocnst@users.noreply.github.com>
2024-01-18 19:35:39 +0200

    Initial Backend

--- DAW_App/src/app/app.module.ts
+++ DAW_App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { MainPanelComponent } from './home/main-panel/main-panel.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DAW_App/src/app/auth/auth-signup/auth-signup.component.ts
+++ DAW_App/src/app/auth/auth-signup/auth-signup.component.ts
@@ -1,18 +1,18 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-auth-signup',
   templateUrl: './auth-signup.component.html',
   styleUrls: ['./auth-signup.component.scss']
 })
 export class AuthSignupComponent {
-  @ViewChild('f', { static: true }) formSign: NgForm;
-
+  @ViewChild('f', { static: true }) formSignUp: NgForm;
 
   onSubmit() {
-    console.log(this.formSign);
-
-    this.formSign.reset();
+    if (this.formSignUp.valid) {
+      console.log(this.formSignUp.value);
+      // Add logic here for what happens when the form is valid and submitted
+      this.formSignUp.reset();
+    }
   }
-
 }
